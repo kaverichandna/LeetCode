1ab07605688ea620ce75708f0872812b2c9a3c09
Kaveri Chandna <89598424+kaverichandna@users.noreply.github.com>
2023-07-02 14:54:18 +0530

    Time: 1913 ms (62.37%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,25 +1,18 @@
-# Write your MySQL query statement below
-
-
 SELECT 
-    product_id, 
-    Sales.year as first_year,
-    quantity,
-    price
-FROM Sales 
-WHERE EXISTS
-(
+  T1.product_id, 
+  first_year, 
+  quantity, 
+  price 
+FROM 
+  (
     SELECT 
-        * 
+      product_id, 
+      MIN(year) as first_year 
     FROM 
-    (
-        SELECT product_id, MIN(year) as min_year
-        FROM Sales s
-        GROUP BY product_id 
-    ) AS T1
-    WHERE 
-        Sales.product_id = T1.product_id 
-        AND 
-        Sales.year = T1.min_year
-) 
-
+      Sales 
+    GROUP BY 
+      product_id
+  ) AS T1 
+  LEFT JOIN Sales T2 ON T1.product_id = T2.product_id 
+WHERE 
+  year = first_year;
